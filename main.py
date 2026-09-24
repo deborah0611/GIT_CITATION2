9cf58e013b7ca0a0c3417d953481576fc7a75f09
Déborah <deborah.oussar@etu.univ-tours.fr>
2026-09-24 11:36:16 +0200

    Ajout avant rich pyfliglet

--- main.py
+++ main.py
@@ -15,3 +15,24 @@ citation = choice(citations)
 
 print(figlet_format("Citation"))
 print(f"[cyan]{citation}[/cyan]")
+
+from random import choice
+
+from pyfiglet import figlet_format
+from rich import print
+from rich.panel import Panel
+
+with open("citations.txt", "r", encoding="utf-8") as f:
+    citations = f.readlines()
+
+citations = [c.strip() for c in citations]
+citation = choice(citations)
+
+print(figlet_format("Citation du jour"))
+print(
+    Panel(
+        f"[italic gold1]« {citation} »[/italic gold1]",
+        border_style="gold1",
+        expand=False ,
+    )
+ )
