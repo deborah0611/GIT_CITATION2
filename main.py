f79b877b30e94907d1433848e06b91ed279a7a74
Déborah <deborah.oussar@etu.univ-tours.fr>
2026-09-24 11:43:30 +0200

    apres richpyfiglet

--- main.py
+++ main.py
@@ -36,3 +36,30 @@ print(
         expand=False ,
     )
  )
+from random import choice
+
+from rich import print
+from rich.panel import Panel
+
+from rich_pyfiglet import RichFiglet
+
+with open("citations.txt", "r", encoding="utf-8") as f:
+    citations = f.readlines()
+
+citations = [c.strip() for c in citations]
+citation = choice(citations)
+
+rich_fig = RichFiglet(
+    "Citation du jour",
+    font="ansi_shadow",
+    colors=["#ff0000", "bright_blue"],
+)
+
+print(rich_fig)
+print(
+    Panel(
+        f"[italic gold1]« {citation} »[/italic gold1]",
+        border_style="gold1",
+        expand=False ,
+    )
+)
